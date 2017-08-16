69bfaa0d75e84465d8a36a0ee9b36411e9180987
Lars Djerf <lars.djerf@gmail.com>
2017-08-16 12:29:15 +0200

    In the gutter watching... the gutter!

---
+++ src/main.rs
@@ -0,0 +1,110 @@
+extern crate rand;
+extern crate sdl2;
+
+use std::f32;
+
+use rand::{Rng, ThreadRng};
+
+use sdl2::pixels::Color;
+use sdl2::rect::Point;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+mod vector;
+
+use vector::Vec2d;
+
+const LUT_RESOLUTION: usize = 1 << 9;
+const NO_OF_STARS: u32 = 512;
+const SCREEN_WIDTH: u32 = 512;
+const SCREEN_HEIGHT: u32 = 512;
+
+#[derive(Debug)]
+struct Star {
+    position: Vec2d,
+    velocity: Vec2d,
+    color: u8,
+}
+
+impl Star {
+    fn randomize(&mut self, rng: &mut ThreadRng) {
+        self.position.x = rng.gen::<f32>() * SCREEN_WIDTH as f32 + SCREEN_WIDTH as f32;
+        self.position.y = rng.gen::<f32>() * SCREEN_HEIGHT as f32;
+        self.velocity.x = -rng.gen::<f32>();
+        self.color = (-self.velocity.x * 255.0) as u8;
+    }
+
+    fn new() -> Star {
+        Star {
+            position: Vec2d { x: 0.0, y: 0.0 },
+            velocity: Vec2d { x: 0.0, y: 0.0 },
+            color: 0,
+        }
+    }
+}
+
+fn generate_stars(rng: &mut ThreadRng) -> Vec<Star> {
+    let mut stars = Vec::new();
+    for _ in 0..NO_OF_STARS {
+        let mut star = Star::new();
+        star.randomize(rng);
+        stars.push(star);
+    }
+
+    stars
+}
+
+fn generate_sin_lut() -> Vec<f32> {
+    let mut lut: Vec<f32> = vec![0.0; LUT_RESOLUTION];
+    let step: f32 = (f32::consts::PI * 2.0) / LUT_RESOLUTION as f32;
+    let mut a: f32 = 0.0;
+    for i in 0..LUT_RESOLUTION {
+        lut[i] = a.sin();
+        a += step;
+    }
+
+    lut
+}
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("J-25", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut rng = rand::thread_rng();
+    let sin_lut = generate_sin_lut();
+    let mut stars = generate_stars(&mut rng);
+
+    'mainloop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'mainloop,
+                _ => {}
+            }
+        }
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        for star in &mut stars {
+            let offset = sin_lut[(star.position.x as usize) & LUT_RESOLUTION - 1] * star.velocity.x * 75.0;
+            let y = star.position.y + offset;
+
+            canvas.set_draw_color(Color::RGB(star.color, star.color, star.color));
+            let _ = canvas.draw_point(Point::new(star.position.x as i32, y as i32));
+
+            star.position = star.position + star.velocity;
+            if star.position.x < 0.0 {
+                star.randomize(&mut rng);
+            }
+        }
+
+        canvas.present();
+    }
+}
---
+++ src/vector.rs
@@ -0,0 +1,122 @@
+use std::ops::{Add, Sub};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Vec2d {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Vec2d {
+    pub fn dot(self, other: Vec2d) -> f32 {
+        self.x * other.x + self.y * other.y
+    }
+
+    pub fn magnitude(&self) -> f32 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    pub fn scale(self, factor: f32) -> Vec2d {
+        Vec2d {
+            x: self.x * factor,
+            y: self.y * factor,
+        }
+    }
+
+    pub fn normalize(&self) -> Vec2d {
+        let m = 1.0 / self.magnitude();
+        self.scale(m)
+    }
+}
+
+impl Add for Vec2d {
+    type Output = Vec2d;
+
+    fn add(self, other: Vec2d) -> Vec2d {
+        Vec2d {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Vec2d {
+    type Output = Vec2d;
+
+    fn sub(self, other: Vec2d) -> Vec2d {
+        Vec2d {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let expected = Vec2d { x: 2.0, y: 4.0 };
+
+        let first = Vec2d { x: 1.0, y: 1.0 };
+        let second = Vec2d { x: 1.0, y: 3.0 };
+
+        assert_eq!(expected, first + second);
+    }
+
+    #[test]
+    fn test_sub() {
+        let expected = Vec2d { x: 0.0, y: -2.0 };
+
+        let first = Vec2d { x: 1.0, y: 1.0 };
+        let second = Vec2d { x: 1.0, y: 3.0 };
+
+        assert_eq!(expected, first - second);
+    }
+
+    #[test]
+    fn test_dot() {
+        let first = Vec2d { x: 2.0, y: 3.0 };
+        let second = Vec2d { x: 4.0, y: 5.0 };
+
+        assert_eq!(first.dot(second), 2.0 * 4.0 + 3.0 * 5.0);
+    }
+
+    #[test]
+    fn test_magnitude() {
+        let a: f32 = 3.0;
+        let b: f32 = 2.0;
+        let vec = Vec2d { x: a, y: b };
+
+        assert_eq!((a * a + b * b).sqrt(), vec.magnitude());
+    }
+
+    #[test]
+    fn test_scale() {
+        let a: f32 = 3.0;
+        let b: f32 = 2.0;
+        let c: f32 = 2.0;
+        let vec = Vec2d { x: a, y: b };
+
+        assert_eq!(Vec2d { x: a * c, y: b * c }, vec.scale(c));
+    }
+
+    #[test]
+    fn test_normalize() {
+        let epsilon = 0.00001;
+        let vectors = vec![
+            Vec2d { x: 0.5, y: 0.5 },
+            Vec2d { x: 1.0, y: 3.2 },
+            Vec2d { x: -2.2, y: 1.0 },
+            Vec2d { x: -1.5, y: -1.5 },
+            Vec2d { x: 5.0, y: 5.0 },
+            Vec2d { x: 1.0, y: 1.0 },
+        ];
+
+        for v in &vectors {
+            let m = v.normalize().magnitude();
+            let rel_err = (1.0 - m) / m;
+            assert!(epsilon > rel_err);
+        }
+    }
+}
